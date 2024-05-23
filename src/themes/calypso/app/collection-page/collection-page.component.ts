df81beb90315ffeef3ff2048eaf9a0aee8d47483
Natalia Jabinschi <natalia.jabinschi@umontreal.ca>
2024-05-23 14:37:46 -0400

    Des modifications ont été apportées pour mettre à jour les sources avec le dépôt distant dspace-angular.

--- src/themes/calypso/app/collection-page/collection-page.component.ts
+++ src/themes/calypso/app/collection-page/collection-page.component.ts
@@ -15,7 +15,7 @@ import {
   fadeInOut,
 } from '../../../../app/shared/animations/fade';
 import { ThemedComcolPageBrowseByComponent } from '../../../../app/shared/comcol/comcol-page-browse-by/themed-comcol-page-browse-by.component';
-import { ComcolPageContentComponent } from '../../../../app/shared/comcol/comcol-page-content/comcol-page-content.component';
+import { ThemedComcolPageContentComponent } from '../../../../app/shared/comcol/comcol-page-content/themed-comcol-page-content.component';
 import { ThemedComcolPageHandleComponent } from '../../../../app/shared/comcol/comcol-page-handle/themed-comcol-page-handle.component';
 import { ComcolPageHeaderComponent } from '../../../../app/shared/comcol/comcol-page-header/comcol-page-header.component';
 import { ComcolPageLogoComponent } from '../../../../app/shared/comcol/comcol-page-logo/comcol-page-logo.component';
@@ -41,10 +41,9 @@ import { VedetteCollListeComponent } from './vedette-coll-liste/vedette-coll-lis
   ],
   standalone: true,
   imports: [
-    ComcolPageContentComponent,
+    ThemedComcolPageContentComponent,
     ErrorComponent,
     NgIf,
-    RouterOutlet,
     ThemedLoadingComponent,
     TranslateModule,
     ViewTrackerComponent,
@@ -56,6 +55,7 @@ import { VedetteCollListeComponent } from './vedette-coll-liste/vedette-coll-lis
     DsoEditMenuComponent,
     ThemedComcolPageBrowseByComponent,
     ObjectCollectionComponent,
+    RouterOutlet,
     VedetteCollListeComponent
   ],
 })
